4aab0e0296db52e8a9582369c567ab433185c038
Oliver Tilke <olivertilke@me.com>
2026-02-25 14:14:25 +0100

    solutuion green

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get "reviews/create"
+  get "restaurants/index"
+  get "restaurants/show"
+  get "restaurants/new"
+  get "restaurants/create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,4 +16,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :restaurants, only: [ :index, :show, :new, :create ] do resources :reviews, only: [ :create ]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Nuking database in 3...2...1..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+Restaurant.create!(name: "Epicure", address: "75008 Paris", phone_number: "01 43 16 30 30", category: "french")
+Restaurant.create!(name: "Mamma Mia", address: "Berlin Mitte", phone_number: "030 123456", category: "italian")
+Restaurant.create!(name: "Sushi Samba", address: "London", phone_number: "+44 20 3000", category: "japanese")
+Restaurant.create!(name: "Peking Ente", address: "Hamburg", phone_number: "040 98765", category: "chinese")
+Restaurant.create!(name: "Frites & Co", address: "Brüssel", phone_number: "0032 123 456", category: "belgian")
+
+puts "Finished! Created #{Restaurant.count} restaurants in the blink on an eye."
